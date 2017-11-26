d89f4c1b86ad53f04bc93cf34df7dba2ba334b17
Hannes Bartle <hannes@michaelbartle.de>
2017-11-26 17:17:51 +0100

    Implement Nearest Class Classifier

--- classifiers/nc_classify.m
+++ classifiers/nc_classify.m
@@ -1,7 +1,49 @@
-function [ labels ] = nc_classify(data_train, data_test, labels_train )
+function [ lables,m ] = nc_classify(data_train, data_test, labels_train )
 %NC_CLASSIFY Nearest Centroid Classifier
-%   Detailed explanation goes here
 
+s = size(data_train);
+
+if s(1) == 784 
+    % MNIST Data
+    % 0-9 Numbers resulting in 10 classes
+    
+    % Calculate Class Mean
+    for i=0:9
+        idx = find(labels_train == i);
+        m(:,i+1)= mean(data_train(:,idx),2);
+    end
+    
+    
+    % Classify the test images
+    [~,number_of_samples] = size(data_test);
+    for i=1:number_of_samples
+        for k =1:10
+            d(k) = norm(data_test(:,i) -m(:,k))^2;
+        end
+        [~,lables(i)] = max(d);  
+    end
+  
+elseif s(1) == 1200
+    % ORL Data
+    % 40 people -> 40 classes
+    
+    % Calculate Class Mean
+    for i=1:40
+        idx = find(labels_train == i);
+        m(:,i)= mean(data_train(:,idx),2);
+    end
+    
+    % Classify the test images
+    [~,number_of_samples] = size(data_test);
+    for i=1:number_of_samples
+        for k =1:40
+            d(k) = norm(data_test(:,i) -m(:,k))^2;
+        end
+        [~,lables(i)] = max(d);  
+    end
+   
+    
+end
 
 end
 
--- machine_learning_project.m
+++ machine_learning_project.m
@@ -14,17 +14,14 @@ clc
 
 %% Set Path Variable
 addpath('misc/')
-addpath('loaders/')
 addpath('classifiers/')
 addpath(genpath('data/'))
 
 %% Load the MNIST Image Data
-
 train_images_MNIST = loadMNISTImages('train-images.idx3-ubyte');
 test_images_MNIST = loadMNISTImages('t10k-images.idx3-ubyte');
 train_labels_MNIST = loadMNISTLabels('train-labels.idx1-ubyte');
 test_labels_MNIST = loadMNISTLabels('t10k-labels.idx1-ubyte');
-
 
 %% Load the ORL Image Data
 load('orl_data.mat')
@@ -34,7 +31,45 @@ clear data
 labels_ORL = lbls;
 clear lbls
 
-%%
+%% Split up ORL Data
+[~,number_of_images]= size(images_ORL);
+
+train_percentage = 0.7;
+train_number = 0.7*number_of_images;
+test_number = number_of_images - train_number;
+
+% Random Permutation
+perm = randperm(number_of_images);
+
+% Shuffle Images
+train_images_ORL = images_ORL(:,perm(1:train_number));
+test_images_ORL = images_ORL(:,perm(train_number+1:end));
+% Shuffle the corresponding labels
+train_labels_ORL = labels_ORL(perm(1:train_number));
+test_labels_ORL = labels_ORL(perm(train_number+1:end));
+
+
+%% Dimensionality Reduction using PCA
+%TODO 
+
+
+
+%% Nearest Class Classifier Evaluation
+
+
+% Classification on Raw Image Data
+[labels_classified_MNIST, means_MNIST] = nc_classify(train_images_MNIST,...
+                                                     test_images_MNIST,...
+                                                     train_labels_MNIST);
+
+[labels_classified_ORL, means_ORL] = nc_classify(train_images_ORL,...
+                                                 test_images_ORL,...
+                                                 train_labels_ORL);
+                          
+                          
+
+                          
+                         
 
 
 
