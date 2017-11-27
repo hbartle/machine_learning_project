2a8f87b5fd80aa448705731e26dde187b1ac4003
Hannes Bartle <hannes@michaelbartle.de>
2017-11-27 19:28:17 +0100

    Implement Nearest Subclass Classifier

--- classifiers/kMeans.m
+++ classifiers/kMeans.m
@@ -1,5 +1,34 @@
-function [ output_args ] = kMeans( input_args )
+function [ labels,centroid] = kMeans(data, K, eta )
 %KMEANS Cluster Data into K Clusters
+if nargin <3
+    eta = 1e-5;
+end
+
+
+[~,number_of_samples] = size(data);
+
+% Random Initial Centroids
+centroid = data(:,randi(number_of_samples,1,K));
+last_centroid = zeros(size(centroid));
+
+d = zeros(1,number_of_samples);
+labels = zeros(1,number_of_samples);
+
+
+% K-Means Clustering
+while norm(centroid - last_centroid) > eta  
+    last_centroid = centroid;
+    for i=1:number_of_samples
+        for l=1:K
+            d(l) = norm(data(:,i) - centroid(:,l))^2;
+        end
+        [~, labels(i)] = min(d);
+    end
+    % Update Centroids
+    for l=1:K
+        centroid(:,l) = mean(data(:,labels==l),2);
+    end
+end
 
 
 end
--- classifiers/nscClassifier.m
+++ classifiers/nscClassifier.m
@@ -1,8 +1,46 @@
-function [ labels ] = nscClassifier(data_train, K, data_test,labels_train )
+function [ labels ] = nscClassifier(data_train,data_test,labels_train,K,data_type)
 %NSC_CLASSIFY Nearest Subclass Centroid Classifier
 
+if strcmp(data_type,'MNIST')
+    % MNIST Data
+    % 0-9 -> 10 classes
+    
+    % Calculate Subclasses 
+    for i=0:9
+        idx = find(labels_train == i);
+        [~,subclass_mean(:,K*i+1:K*(i+1))] = kMeans(data_train(:,idx),K);
+    end
+    
+    
+    % Classify the test images
+    [~,number_of_samples] = size(data_test);
+    for i=1:number_of_samples
+        for k =1:10*K
+            d(k) = norm(data_test(:,i) - subclass_mean(:,k))^2;
+        end
+        [~,labels(i)] = min(d);
+        labels(i) = ceil(labels(i)/K);
+    end
+elseif strcmp(data_type,'ORL')
+    % ORL Data
+    % 40 Classes
+    for i=1:40
+        idx = find(labels_train == i);
+        [~,subclass_mean(:,K*(i-1)+1:K*(i))] = kMeans(data_train(:,idx),K);
+    end
+    
+    
+    % Classify the test images
+    [~,number_of_samples] = size(data_test);
+    for i=1:number_of_samples
+        for k =1:40*K
+            d(k) = norm(data_test(:,i) - subclass_mean(:,k))^2;
+        end
+        [~,labels(i)] = min(d);
+        labels(i) = ceil(labels(i)/K);
+    end
 
 
-
+end
 end
 
