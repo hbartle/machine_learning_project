66d372bc334e0bf72f89472f655e51c31ff338b1
Hannes Bartle <hannes@michaelbartle.de>
2017-11-28 21:38:32 +0100

    Restructure Classifier Code

--- classifiers/ncClassifier.m
+++ classifiers/ncClassifier.m
@@ -5,44 +5,31 @@ function [ lables,m ] = ncClassifier(data_train, data_test, labels_train, data_t
 if strcmp(data_type,'MNIST')  
     % MNIST Data
     % 0-9 Numbers resulting in 10 classes
-    
-    % Calculate Class Mean
-    for i=0:9
-        idx = find(labels_train == i);
-        m(:,i+1)= mean(data_train(:,idx),2);
-    end
-    
-    
-    % Classify the test images
-    [~,number_of_samples] = size(data_test);
-    for i=1:number_of_samples
-        for k =1:10
-            d(k) = norm(data_test(:,i) -m(:,k))^2;
-        end
-        [~,lables(i)] = min(d);  
-    end
-  
+    number_of_classes= 10;
+    label_correction = 1;
 elseif strcmp(data_type,'ORL') 
     % ORL Data
-    % 40 people -> 40 classes
-    
-    % Calculate Class Mean
-    for i=1:40
-        idx = find(labels_train == i);
-        m(:,i)= mean(data_train(:,idx),2);
-    end
-    
-    % Classify the test images
-    [~,number_of_samples] = size(data_test);
-    for i=1:number_of_samples
-        for k =1:40
-            d(k) = norm(data_test(:,i) -m(:,k))^2;
-        end
-        [~,lables(i)] = min(d);  
-    end
-   
-    
+    % 40 Classes
+    number_of_classes= 40;
+    label_correction = 0;  
 end
+
+% Calculate Class Mean
+for i=1:number_of_classes
+    idx = find(labels_train == i-label_correction);
+    m(:,i)= mean(data_train(:,idx),2);
+end
+
+
+% Classify the test images
+[~,number_of_samples] = size(data_test);
+for i=1:number_of_samples
+    for k =1:number_of_classes
+        d(k) = norm(data_test(:,i) -m(:,k))^2;
+    end
+    [~,lables(i)] = min(d);  
+end
+
 
 end
 
--- classifiers/nscClassifier.m
+++ classifiers/nscClassifier.m
@@ -1,46 +1,36 @@
 function [ labels ] = nscClassifier(data_train,data_test,labels_train,K,data_type)
 %NSC_CLASSIFY Nearest Subclass Centroid Classifier
 
-if strcmp(data_type,'MNIST')
+if strcmp(data_type,'MNIST')  
     % MNIST Data
-    % 0-9 -> 10 classes
-    
-    % Calculate Subclasses 
-    for i=0:9
-        idx = find(labels_train == i);
-        [~,subclass_mean(:,K*i+1:K*(i+1))] = kMeans(data_train(:,idx),K);
-    end
-    
-    
-    % Classify the test images
-    [~,number_of_samples] = size(data_test);
-    for i=1:number_of_samples
-        for k =1:10*K
-            d(k) = norm(data_test(:,i) - subclass_mean(:,k))^2;
-        end
-        [~,labels(i)] = min(d);
-        labels(i) = ceil(labels(i)/K);
-    end
-elseif strcmp(data_type,'ORL')
+    % 0-9 Numbers resulting in 10 classes
+    number_of_classes= 10;
+    label_correction = 1;
+elseif strcmp(data_type,'ORL') 
     % ORL Data
     % 40 Classes
-    for i=1:40
-        idx = find(labels_train == i);
-        [~,subclass_mean(:,K*(i-1)+1:K*(i))] = kMeans(data_train(:,idx),K);
-    end
-    
-    
-    % Classify the test images
-    [~,number_of_samples] = size(data_test);
-    for i=1:number_of_samples
-        for k =1:40*K
-            d(k) = norm(data_test(:,i) - subclass_mean(:,k))^2;
-        end
-        [~,labels(i)] = min(d);
-        labels(i) = ceil(labels(i)/K);
-    end
-
-
+    number_of_classes= 40;
+    label_correction = 0;  
 end
+
+    
+% Calculate Subclasses 
+for i=1:number_of_classes
+    idx = find(labels_train == i-label_correction);
+    [~,subclass_mean(:,K*(i-1)+1:K*(i))] = kMeans(data_train(:,idx),K);
+end
+
+
+% Classify the test images
+[~,number_of_samples] = size(data_test);
+for i=1:number_of_samples
+    for k =1:10*K
+        d(k) = norm(data_test(:,i) - subclass_mean(:,k))^2;
+    end
+    [~,labels(i)] = min(d);
+    labels(i) = ceil(labels(i)/K);
+end
+
+
 end
 
