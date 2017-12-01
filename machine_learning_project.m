9accb9913136e3b685e06b88b2c96dedb2194325
Hannes Bartle <hannes@michaelbartle.de>
2017-12-01 09:55:46 +0100

    Add timing measurment and refactor scoring

--- machine_learning_project.m
+++ machine_learning_project.m
@@ -63,171 +63,183 @@ test_images_ORL_pca = principalComponents(test_images_ORL,...
 
 
 %% Nearest Class Classifier Evaluation
-
-
+tic;
 % Classification on Raw Image Data
 nc_labels_MNIST = ncClassifier(train_images_MNIST(:,:),...
                                test_images_MNIST(:,:),...
                                train_labels_MNIST(:),...
                                'MNIST');
+t_nc_MNIST = toc;
 
+tic;
 nc_labels_ORL = ncClassifier(train_images_ORL,...
                              test_images_ORL,...
                              train_labels_ORL,...
                              'ORL');
+t_nc_ORL = toc;
 
-
-
-scoreClassifier(nc_labels_MNIST,test_labels_MNIST(:))
-scoreClassifier(nc_labels_ORL,test_labels_ORL)
-
-%% Classification on PCA-reduced Image Data
+% Classification on PCA-reduced Image Data
+tic;
 nc_labels_MNIST_pca = ncClassifier(train_images_MNIST_pca,...
                                                   test_images_MNIST_pca,...
                                                   train_labels_MNIST,...
                                                   'MNIST');
-
+t_nc_MNIST_pca = toc;
+tic;
 nc_labels_ORL_pca = ncClassifier(train_images_ORL_pca,...
                                               test_images_ORL_pca,...
                                               train_labels_ORL,...
                                               'ORL');
-                          
-                          
-
-scoreClassifier(nc_labels_MNIST_pca,test_labels_MNIST)
-scoreClassifier(nc_labels_ORL_pca,test_labels_ORL)
-                         
-
+t_nc_ORL_pca = toc;
 %% Nearest Subclass Classifier
 
 % Classification on Raw Image Data
+tic;
 nsc_labels_MNIST = nscClassifier(train_images_MNIST,...
                                test_images_MNIST,...
                                train_labels_MNIST,...
                                5,...
                                'MNIST');
+t_nsc_MNIST = toc;
+tic;
 nsc_labels_ORL = nscClassifier(train_images_ORL,...
                                test_images_ORL,...
                                train_labels_ORL,...
                                5,...
                                'ORL');
+t_nsc_ORL = toc;
 
-scoreClassifier(nsc_labels_MNIST,test_labels_MNIST)
-scoreClassifier(nsc_labels_ORL,test_labels_ORL)
-
-%% Classification on PCA-reduced Image Data
+% Classification on PCA-reduced Image Data
+tic;
 nsc_labels_MNIST_pca = nscClassifier(train_images_MNIST_pca,...
                                test_images_MNIST_pca,...
                                train_labels_MNIST,...
                                2,...
                                'MNIST');
+t_nsc_MNIST_pca = toc;
+tic;
 nsc_labels_ORL_pca = nscClassifier(train_images_ORL_pca,...
                                test_images_ORL_pca,...
                                train_labels_ORL,...
                                2,...
                                'ORL');
-
-scoreClassifier(nsc_labels_MNIST_pca,test_labels_MNIST)
-scoreClassifier(nsc_labels_ORL_pca,test_labels_ORL)
-
+t_nsc_ORL_pca = toc;
 
 %% Nearest Neighborhood Classifier
 
 % Classification on Raw Image Data
 training_subset = 700;
 testing_subset = 300;
+tic;
 nn_labels_MNIST = nnClassifier(train_images_MNIST(:,1:training_subset),...
                                test_images_MNIST(:,1:testing_subset),...
                                train_labels_MNIST(1:training_subset));
-scoreClassifier(nn_labels_MNIST,test_labels_MNIST(1:testing_subset))
-  
+t_nn_MNIST = toc;
+tic;
 nn_labels_ORL = nnClassifier(train_images_ORL,...
                                test_images_ORL,...
                                train_labels_ORL);
-scoreClassifier(nn_labels_ORL,test_labels_ORL)
-%%
+t_nn_ORL = toc;
+
 % Classification on PCA-reduced Image Data
+tic;
 nn_labels_MNIST_pca = nnClassifier(train_images_MNIST_pca(:,1:training_subset),...
                                test_images_MNIST_pca(:,1:testing_subset),...
                                train_labels_MNIST(1:training_subset));
-scoreClassifier(nn_labels_MNIST_pca,test_labels_MNIST(1:testing_subset))
-
+t_nn_MNIST_pca = toc;
+tic;
 nn_labels_ORL_pca = nnClassifier(train_images_ORL_pca,...
                                test_images_ORL_pca,...
                                train_labels_ORL);
-scoreClassifier(nn_labels_ORL_pca,test_labels_ORL)
-
-
+t_nn_ORL_pca = toc;
 %% Perceptron with Backpropagation on original Data
-
+tic;
 W_MNIST = trainPerceptronBP(train_images_MNIST,...
                       train_labels_MNIST,...
                       0.1,...
-                      'MNIST');
-                  
-                  
+                      'MNIST');           
 pBP_labels_MNIST = perceptronBP(W_MNIST,test_images_MNIST,'MNIST');
-                  
-scoreClassifier(pBP_labels_MNIST,test_labels_MNIST)
-                 
-      
+t_pbp_MNIST = toc;
+tic;                  
 W_ORL = trainPerceptronBP(train_images_ORL,...
                       train_labels_ORL,...
                       0.1,...
                       'ORL'); 
 pBP_labels_ORL = perceptronBP(W_ORL,test_images_ORL,'ORL');
+t_pbp_ORL = toc;
 
-scoreClassifier(pBP_labels_ORL,test_labels_ORL)
-
-%% Perceptron with Backpropagation on PCA-reduced Data
-
+% Perceptron with Backpropagation on PCA-reduced Data
+tic;
 W_MNIST_pca = trainPerceptronBP(train_images_MNIST_pca,...
                       train_labels_MNIST,...
                       0.1,...
-                      'MNIST');
-                  
+                      'MNIST');              
 pBP_labels_MNIST_pca = perceptronBP(W_MNIST_pca,test_images_MNIST_pca,'MNIST');
-                  
-scoreClassifier(pBP_labels_MNIST_pca,test_labels_MNIST)
-                 
-   
+t_pbp_MNIST_pca = toc;                               
+tic;
 W_ORL_pca = trainPerceptronBP(train_images_ORL_pca,...
                       train_labels_ORL,...
                       0.1,...
                       'ORL'); 
 pBP_labels_ORL_pca = perceptronBP(W_ORL_pca,test_images_ORL_pca,'ORL');
-
-scoreClassifier(pBP_labels_ORL_pca,test_labels_ORL)
+t_pbp_ORL_pca = toc;
 
 %% Perceptron Minimum Square Error
 
 % Original Data
+tic;
 W_MNIST_MSE = trainPerceptronMSE(train_images_MNIST,...
                               train_labels_MNIST,'MNIST'); 
 pMSE_labels_MNIST = perceptronMSE(W_MNIST_MSE,test_images_MNIST,'MNIST');
-
-scoreClassifier(pMSE_labels_MNIST,test_labels_MNIST)
-
+t_pmse_MNIST = toc;
+tic;
 W_ORL_MSE = trainPerceptronMSE(train_images_ORL,...
                               train_labels_ORL,'ORL'); 
 pMSE_labels_ORL = perceptronMSE(W_ORL_MSE,test_images_ORL,'ORL');
+t_pmse_ORL = toc;
 
-scoreClassifier(pMSE_labels_ORL,test_labels_ORL)
-
-%% PCA-reduced Data
+% PCA-reduced Data
+tic;
 W_MNIST_MSE_pca = trainPerceptronMSE(train_images_MNIST_pca,...
                               train_labels_MNIST,'MNIST'); 
 pMSE_labels_MNIST_pca = perceptronMSE(W_MNIST_MSE_pca,...
                                       test_images_MNIST_pca,'MNIST');
-
-scoreClassifier(pMSE_labels_MNIST_pca,test_labels_MNIST)
-
+t_pmse_MNIST_pca = toc;
+tic;
 W_ORL_MSE_pca = trainPerceptronMSE(train_images_ORL_pca,...
                               train_labels_ORL,'ORL'); 
 pMSE_labels_ORL_pca = perceptronMSE(W_ORL_MSE_pca,test_images_ORL_pca,'ORL');
+t_pmse_ORL_pca = toc;     
 
-scoreClassifier(pMSE_labels_ORL_pca,test_labels_ORL)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Scoring
 
-         
+% Nearest Centroid
+sc_nc_MNIST = scoreClassifier(nc_labels_MNIST,test_labels_MNIST(:));
+sc_nc_ORL   = scoreClassifier(nc_labels_ORL,test_labels_ORL);
+sc_nc_MNIST_pca = scoreClassifier(nc_labels_MNIST_pca,test_labels_MNIST);
+sc_nc_ORL_pca   = scoreClassifier(nc_labels_ORL_pca,test_labels_ORL);
 
+% Nearest Subclass
+sc_nsc_MNIST = scoreClassifier(nsc_labels_MNIST,test_labels_MNIST);
+sc_nsc_ORL   = scoreClassifier(nsc_labels_ORL,test_labels_ORL);
+sc_nsc_MNIST_pca = scoreClassifier(nsc_labels_MNIST_pca,test_labels_MNIST);
+sc_nsc_ORL_pca   = scoreClassifier(nsc_labels_ORL_pca,test_labels_ORL);
 
+% Nearest Neighbor
+sc_nn_MNIST = scoreClassifier(nn_labels_MNIST,test_labels_MNIST(1:testing_subset));
+sc_nn_ORL   = scoreClassifier(nn_labels_ORL,test_labels_ORL);
+sc_nn_MNIST_pca = scoreClassifier(nn_labels_MNIST_pca,test_labels_MNIST(1:testing_subset));
+sc_nn_ORL_pca   = scoreClassifier(nn_labels_ORL_pca,test_labels_ORL);
+
+% BP Perceptron
+sc_pbp_MNIST = scoreClassifier(pBP_labels_MNIST,test_labels_MNIST);
+sc_pbp_ORL   = scoreClassifier(pBP_labels_ORL,test_labels_ORL);
+sc_pbp_MNIST_pca = scoreClassifier(pBP_labels_MNIST_pca,test_labels_MNIST);
+sc_pbp_ORL_pca   = scoreClassifier(pBP_labels_ORL_pca,test_labels_ORL);
+
+% MSE Perceptron
+sc_pmse_MNIST = scoreClassifier(pMSE_labels_MNIST,test_labels_MNIST);
+sc_pmse_ORL   = scoreClassifier(pMSE_labels_ORL,test_labels_ORL);
+sc_pmse_MNIST_pca = scoreClassifier(pMSE_labels_MNIST_pca,test_labels_MNIST);
+sc_pmse_ORL_pca   = scoreClassifier(pMSE_labels_ORL_pca,test_labels_ORL);
